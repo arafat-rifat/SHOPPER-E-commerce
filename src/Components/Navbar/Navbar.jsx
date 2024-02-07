152729f4e4d54efb9325033fbcbbc8e877f2d10d
arafat-rifat <arafat15-12603@diu.edu.bd>
2024-02-07 21:41:38 +0600

    Project Done For FrontEnd PART

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,13 @@
-import React, { useState } from "react";
+import React, { useState,useContext } from "react";
 import logo from "../Assets/logo.png";
 import cart_icon from "../Assets/cart_icon.png";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
+import { ShopContext } from "../../Context/ShopContext";
 
 const Navbar = () => {
   const [menu, setMenu] = useState("shop");
+  const { getTotalCartItems } = useContext(ShopContext);
 
   return (
     <div className="navbar">
@@ -23,37 +25,45 @@ const Navbar = () => {
             setMenu("shop");
           }}
         >
-          <Link to={'/'}>Shop</Link>{menu === "shop" ? <hr /> : <></>}
+          <Link to={"/"}>Shop</Link>
+          {menu === "shop" ? <hr /> : <></>}
         </li>
         <li
           onClick={() => {
             setMenu("men");
           }}
         >
-          <Link to={'/mens'}>Men</Link>{menu === "men" ? <hr /> : <></>}
+          <Link to={"/mens"}>Men</Link>
+          {menu === "men" ? <hr /> : <></>}
         </li>
         <li
           onClick={() => {
             setMenu("women");
           }}
         >
-          <Link to={'/womens'}>Women</Link>{menu === "women" ? <hr /> : <></>}
+          <Link to={"/womens"}>Women</Link>
+          {menu === "women" ? <hr /> : <></>}
         </li>
         <li
           onClick={() => {
             setMenu("kids");
           }}
         >
-          <Link to={'/kids'}>Kids</Link>{menu === "kids" ? <hr /> : <></>}
+          <Link to={"/kids"}>Kids</Link>
+          {menu === "kids" ? <hr /> : <></>}
         </li>
       </ul>
 
       {/* This is for Login and Cart Icon */}
 
       <div className="nav-login-cart">
-        <Link to={'/login'}><button>Login</button></Link>
-        <Link to={'/cart'}><img src={cart_icon} alt="carticon" /></Link>
-        <div className="nav-cart-count">0</div>
+        <Link to={"/login"}>
+          <button>Login</button>
+        </Link>
+        <Link to={"/cart"}>
+          <img src={cart_icon} alt="carticon" />
+        </Link>
+        <div className="nav-cart-count">{getTotalCartItems()}</div>
       </div>
     </div>
   );
